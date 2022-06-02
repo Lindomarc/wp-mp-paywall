56ee2c25280772247d454ec4cdb18429d9efe4b7
Lindomar Corsino <lindomarc@outlook.com>
2022-06-01 21:48:19 -0300

    fix: ajuste pagina planos

--- modules/shortcodes/views/plans.php
+++ modules/shortcodes/views/plans.php
@@ -7,7 +7,8 @@
                 <div class="pdi-paywall-header  panel panel-header panel-info ">
                     <h3 class="panel-title"><?php print $plan['reason']; ?></h3>
                 </div>
-                <div class="pdi-paywall-price panel-body">
+                <div class="panel-body">
+                <div class="pdi-paywall-price">
                     <?php list($real, $cent) = explode(',', number_format($plan['auto_recurring']['transaction_amount'], 2, ',', '')); ?>
                     <p><span class="signal">R$</span><span class="real"> <?php echo $real ?></span><span
                                 class="cent">,<?php echo $cent; ?></span>
@@ -17,7 +18,8 @@
                         <p><?php print nl2br($plan['description']); ?></p>
                     </div>
                 </div>
-                <div class="pdi-paywall-payment panel-footer">
+                </div>
+                <div class="pdi-paywall-payment">
                     <div class="pdi-paywall-payment-button">
                         <a href="<?php echo $page_register ?>?plan=<?php echo base64_encode($plan['extern_plan_id']); ?>">
                             Assinar
@@ -28,4 +30,18 @@
         <?php } ?>
     <?php } ?>
 </div>
+<style>
+
+.panel-body {
+    padding: 0!important;
+    height: calc(100% - 200px)!important;
+}
+.panel-footer{
+    background-color: transparent!important;
+}
+header.entry-header{
+    display: none!important;
+}
+
+</style>
 
