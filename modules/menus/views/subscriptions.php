133222c209d05fa41c4e7ad7a6b474cbf10f9e23
Lindomar Corsino <lindomarc@outlook.com>
2022-07-06 22:54:25 -0300

    fix: layout pdi-paywall-subscriptions

--- modules/menus/views/subscriptions.php
+++ modules/menus/views/subscriptions.php
@@ -1,9 +1,8 @@
 <link rel="stylesheet" type="text/css" href="https://cdn.datatables.net/1.12.1/css/jquery.dataTables.css">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css"/>
+<link href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" rel="stylesheet">
 <script type="text/javascript" charset="utf8" src="https://cdn.datatables.net/1.12.1/js/jquery.dataTables.js"></script>
 <script>
-    // jQuery(document).ready( function () {
-    //     jQuery('#table_subscribers').DataTable();
-    // } );
     jQuery(document).ready(function () {
         jQuery('#table_subscribers').DataTable({
             language: {
@@ -17,82 +16,91 @@
                     xhr.setRequestHeader('x-customer-key', '<?php echo get_option('_pdi_paywall_payment_pdi_key'); ?>');
                 },
             },
+            'responsive': true,
             'processing': true,
             'serverSide': true,
             'serverMethod': 'post',
-            'pageLength': 25,
+            'pageLength': 10,
             columns: [
-                {data: 'first_name'},
+                {data: 'first_name', name: 'first_name'},
                 {data: 'payer_email'},
                 {data: 'reason'},
                 {
                     data: 'transaction_amount',
-                    render: function(data, type, row){
-                        if(type === "sort" || type === "type"){
+                    render: function (data, type, row) {
+                        if (type === "sort" || type === "type") {
                             return data;
                         }
                         let value = parseFloat(data);
-                        return value.toLocaleString('pt-BR',{style: 'currency', currency: 'BRL'})
+                        return value.toLocaleString('pt-BR', {style: 'currency', currency: 'BRL'})
                     }
                 },
                 {
                     data: 'status',
-                    render: function(data, type, row){
-                        if(type === "sort" || type === "type"){
+                    render: function (data, type, row) {
+                        if (type === "sort" || type === "type") {
                             return data;
                         }
                         let status;
                         switch (data) {
                             case 'cancelled':
-                                status =  'Cancelado'
+                                status = 'Cancelado'
                                 break;
                             case 'authorized':
                                 status = 'Autorizado'
                                 break;
                             default:
-                                status =  data;
+                                status = data;
                         }
                         return status
                     }
                 },
                 {
                     data: 'next_retry_date',
-                    render: function(data, type, row){
-                            if(type === "sort" || type === "type"){
-                                return data;
-                            }
+                    render: function (data, type, row) {
+                        if (type === "sort" || type === "type") {
+                            return data;
+                        }
                         let date = new Date(data)
-                        return   date.toLocaleString('pt-BR',{timeZone:'UTC'})
+                        return date.toLocaleString('pt-BR', {timeZone: 'UTC'})
                     }
                 },
             ],
         });
     });
 </script>
-<div class="wrap">
-    <div class="container">
-        <h2>PDI Paywall - Assinantes</h2>
-        <table id="table_subscribers" class="table table-bordered data-table wp-list-table widefat fixed striped table-view-list posts">
-            <thead>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Plano</th>
-                    <th>Valor</th>
-                    <th>Situação</th>
-                    <th>Próximo pagamento</th>
-                </tr>
-            </thead>
-            <tfoot>
-                <tr>
-                    <th>Nome</th>
-                    <th>Email</th>
-                    <th>Plano</th>
-                    <th>Valor</th>
-                    <th>Situação</th>
-                    <th>Próximo pagamento</th>
-                </tr>
-            </tfoot>
-        </table>
-    </div>
+
+<div class="container">
+    <h1 class="mb-5 mt-5">PDI Paywall - Assinantes</h1>
+    <table id="table_subscribers" class="table table-bordered data-table responsive nowrap">
+        <thead>
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Plano</th>
+            <th>Valor</th>
+            <th>Situação</th>
+            <th>Próximo pagamento</th>
+        </tr>
+        </thead>
+        <tfoot>
+        <tr>
+            <th>Nome</th>
+            <th>Email</th>
+            <th>Plano</th>
+            <th>Valor</th>
+            <th>Situação</th>
+            <th>Próximo pagamento</th>
+        </tr>
+        </tfoot>
+    </table>
 </div>
+
+<style>
+    #table_subscribers {
+        width: 100% !important;
+    }
+    .container {
+        max-width: 100%;
+    }
+</style>
